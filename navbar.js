8bdd23b41bfd0bf2c53e2a67ad53a528f3950a8c
Sean Dowdle <dowdle_sean@live.com>
2020-04-02 00:27:12 -0700

    Added dynamic text color changing on navbar

--- navbar.js
+++ navbar.js
@@ -1,9 +1,15 @@
 /* ~~~~~~~~~~~~~~~~~~~ Nav Bar ~~~~~~~~~~~~~~~~~~~ */
-
+// For sticking the navbar
 const navbar = document.querySelector('nav');
 const navbarParent = navbar.parentElement;
 const navbarSize = navbar.offsetHeight;
 var navPosition = navbar.offsetTop;
+// For changing navbar colors based on scroll position
+var headerSize = $('header').outerHeight();
+var aboutmeSize = $('#about-me').outerHeight();
+var educationSize = $('#education').outerHeight();
+var experienceSize = $('#experience').outerHeight();
+var projectSize = $('#projects').outerHeight();
 
 // Helper function to determine if the navbar needs to be stickied or not
 function stickyNav() {
@@ -20,9 +26,66 @@ function stickyNav() {
 $(window).resize(function() {
   navPosition = navbarParent.offsetHeight - navbarSize;
   stickyNav();
+  headerSize = $('header').outerHeight();
+  aboutmeSize = $('#about-me').outerHeight();
+  educationSize = $('#education').outerHeight();
+  experienceSize = $('#experience').outerHeight();
+  projectSize = $('#projects').outerHeight();
 });
 
 // Add listener for scrolling and call the function on event
 $(window).scroll(function() {
   stickyNav();
+
+  // Now detect where the user is on the page so we can dynamically change the color of the navbar text
+  var midOfScreen = Math.floor($(window).height() / 2);
+  var scrollPosition = window.scrollY + midOfScreen;
+
+  var navAboutText = document.getElementById('nav-about');
+  var navEduText = document.getElementById('nav-edu');
+  var navExpText = document.getElementById('nav-exp');
+  var navProjText = document.getElementById('nav-proj');
+
+  if(scrollPosition >= headerSize && scrollPosition < headerSize + aboutmeSize)
+  {
+    // In About Me section
+    navAboutText.setAttribute('style', 'color: white;');
+    navEduText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navExpText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navProjText.setAttribute('style', 'color: rgb(172, 172, 172);');
+  }
+  else if(scrollPosition >= headerSize + aboutmeSize && scrollPosition < headerSize + aboutmeSize + educationSize)
+  {
+    // In Education section
+    navAboutText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navEduText.setAttribute('style', 'color: white;');
+    navExpText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navProjText.setAttribute('style', 'color: rgb(172, 172, 172);');
+  }
+  else if(scrollPosition >= headerSize + aboutmeSize + educationSize 
+          && scrollPosition < headerSize + aboutmeSize + educationSize + experienceSize)
+  {
+    // In Experience section
+    navAboutText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navEduText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navExpText.setAttribute('style', 'color: white;');
+    navProjText.setAttribute('style', 'color: rgb(172, 172, 172);');
+  }
+  else if(scrollPosition >= headerSize + aboutmeSize + educationSize + experienceSize
+          && scrollPosition < headerSize + aboutmeSize + educationSize + experienceSize + projectSize)
+  {
+    // In Projects section
+    navAboutText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navEduText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navExpText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navProjText.setAttribute('style', 'color: white;');
+  }
+  else{
+    navAboutText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navEduText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navExpText.setAttribute('style', 'color: rgb(172, 172, 172);');
+    navProjText.setAttribute('style', 'color: rgb(172, 172, 172);');
+  }
 });
+
+/* ~~~~~~~~~~~~~~~~~~~ Nav Bar Handling Offset ~~~~~~~~~~~~~~~~~~~ */
